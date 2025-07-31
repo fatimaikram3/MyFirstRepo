27dc66b555f61a074805ff2a87ea8b88fff7d79f
Fatima Ikram <fatimaikram3127@gmail.com>
2025-07-31 11:51:52 -0700

    Add files via upload

---
+++ program.py
@@ -0,0 +1,42 @@
+#addition
+x=2
+y=8
+result=x+y
+print("addition:",result)
+
+
+#subtraction
+x=10
+y=4
+result=x-y
+print("subtraction:",result)
+
+#multiplication
+a=6
+b=4
+result=a*b
+print("multiplication:",result)
+
+#division
+a=5
+b=5
+result=a/b
+print("division:",result)
+
+#floor division
+a=7
+b=2
+result= a // b
+print("floor division:",result)
+
+#modulus
+a=5
+b=2
+result=a%b
+print("modulus:",result)
+
+#exponentiation **
+a=2
+b=3
+result=a**b
+print("exponentiation:",result)
